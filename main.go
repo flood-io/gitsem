21db90a7aeb40cbfd41eec9e597af50c0b61638a
Lachie Cox <lachiec@gmail.com>
2018-02-08 09:55:09 +1100

    usability tweaks
    - -preview prints what the version change would be without modifying the
    repo
    - -n skips the clean check
    - trim "v" from the literal semver argument if it's there
    - move some error checking earlier to fail faster

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"gopkg.in/blang/semver.v1"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+
+	semver "gopkg.in/blang/semver.v1"
 )
 
 func commitMessage(message, version string) string {
@@ -56,71 +57,108 @@ func bump(old *semver.Version, part string) *semver.Version {
 	return new
 }
 
+type context struct {
+	versionFile      string
+	oldVersion       *semver.Version
+	newVersion       *semver.Version
+	shouldCleanCheck bool
+	message          string
+	shouldTag        bool
+	preview          bool
+}
+
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of %s: [options] version\n\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "version can be one of: newversion | patch | minor | major\n\n")
+		fmt.Fprintf(os.Stderr, "version can be one of: <literal semver> | patch | minor | major\n\n")
 		fmt.Fprintf(os.Stderr, "options:\n")
 		flag.PrintDefaults()
 	}
-	message := flag.String("m", "%s", "commit message for version commit")
+	var ctx context
+	flag.StringVar(&ctx.message, "m", "%s", "commit message for version commit")
 	help := flag.Bool("h", false, "print usage and exit")
-	shouldTag := flag.Bool("tag", true, "whether or not to make a tag at the version commit")
+	flag.BoolVar(&ctx.shouldTag, "tag", true, "whether or not to make a tag at the version commit")
+	flag.BoolVar(&ctx.preview, "preview", false, "whether to show a preview of the version without modifying anything")
+	noCleanCheck := flag.Bool("n", false, "whether to check git repo's cleanliness before proceeding")
 	flag.Parse()
+
+	ctx.shouldCleanCheck = !*noCleanCheck
 
 	if *help {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	if *message == "" {
+	if ctx.message == "" {
 		exitWithError("missing message")
 	}
 
-	if clean, err := isRepoClean(); err != nil {
-		log.Fatal(err)
-	} else if !clean {
-		log.Fatal("repo isn't clean")
+	if len(flag.Args()) != 1 {
+		exitWithError("gitsem takes exactly one non-flag argument: version")
 	}
 
 	root, err := repoRoot()
 	if err != nil {
 		log.Fatal(err)
 	}
-	versionFile := filepath.Join(root, versionFileName)
-	version, err := getCurrentVersion(versionFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if len(flag.Args()) != 1 {
-		exitWithError("gitsem takes exactly one non-flag argument: version")
-	}
 
-	newVersion := flag.Args()[0]
-	switch newVersion {
-	case "patch", "minor", "major":
-		version = bump(version, newVersion)
-	default:
-		if version, err = semver.New(newVersion); err != nil {
-			log.Fatalf("failed to parse %s as semver: %s", newVersion, err.Error())
+	if ctx.shouldCleanCheck {
+		if clean, err := isRepoClean(); err != nil {
+			log.Fatal(err)
+		} else if !clean {
+			log.Fatal("repo isn't clean")
 		}
 	}
 
-	if err := ioutil.WriteFile(versionFile, []byte(version.String()), 0666); err != nil {
+	ctx.versionFile = filepath.Join(root, versionFileName)
+	version, err := getCurrentVersion(ctx.versionFile)
+	if err != nil {
 		log.Fatal(err)
 	}
-	if err := addFile(versionFile); err != nil {
+	ctx.oldVersion = version
+
+	newVersionAction := flag.Args()[0]
+	switch newVersionAction {
+	case "patch", "minor", "major":
+		version = bump(version, newVersionAction)
+	default: // newVersionAction is an actual version string
+		if strings.HasPrefix(newVersionAction, "v") {
+			newVersionAction = strings.TrimPrefix(newVersionAction, "v")
+		}
+		if version, err = semver.New(newVersionAction); err != nil {
+			log.Fatalf("failed to parse %s as semver: %s", newVersionAction, err.Error())
+		}
+	}
+	ctx.newVersion = version
+
+	if ctx.preview {
+		printPreview(ctx)
+	} else {
+		commitNewVersion(ctx)
+	}
+}
+
+func commitNewVersion(ctx context) {
+	if err := ioutil.WriteFile(ctx.versionFile, []byte(ctx.newVersion.String()), 0666); err != nil {
 		log.Fatal(err)
 	}
-	versionString := "v" + version.String()
-	*message = commitMessage(*message, versionString)
-	if err := commit(*message); err != nil {
+	if err := addFile(ctx.versionFile); err != nil {
 		log.Fatal(err)
 	}
-	if *shouldTag {
+	versionString := "v" + ctx.newVersion.String()
+	message := commitMessage(ctx.message, versionString)
+	if err := commit(message); err != nil {
+		log.Fatal(err)
+	}
+	if ctx.shouldTag {
 		if err := tag(versionString); err != nil {
 			log.Fatal(err)
 		}
 	}
 	fmt.Println(versionString)
+}
+
+func printPreview(ctx context) {
+	fmt.Printf("current version: v%s\n", ctx.oldVersion)
+	fmt.Printf("    new version: v%s\n", ctx.newVersion)
 }
